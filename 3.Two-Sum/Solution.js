6b0763e45dd532cb99f80b0538f59ba0baa2cbe4
Adi1336 <adityapatil1336@gmail.com>
2025-08-10 22:48:35 +0530

    3.Two sum

---
+++ 3.Two-Sum/Solution.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const hash = new Map()
+
+    for(let i=0;i<nums.length;i++){
+        let complement = target - nums[i];
+
+        if(hash.has(complement)){
+            return [hash.get(complement),i];
+        }
+
+        hash.set(nums[i],i);
+    }
+    return [];
+};
